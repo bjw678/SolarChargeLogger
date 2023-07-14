ec58ffe401f49982ab6a25a3520387bbc3c48203
bjw <bjw678@hotmail.com>
2023-07-14 11:01:44 +1000

    initial development

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SolarChargeLogger
-Logs battery voltage and charge current from a solar cell into a lead acid battery to an SD card and optionally the serial output
+Logs battery voltage and charge current from a solar cell into a lead acid battery to an SD card and optionally the serial output.
+
+To measure voltage and current a voltage divider is placed across the voltage as defined by R1 and R2 defines.
+Current is measured with a current shunt of resistance specefied in the defines.
+Voltage is measured in millivolts to increase precision without floating point math.
+Current is in Ma for the same reason. a smaller unit may be worthwhile.
---
+++ solarChargeLogger/solarChargeLogger.ino
@@ -0,0 +1,163 @@
+
+#include <SD.h>
+#include <SPI.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
+
+#define SD_CS_PIN 10           // Chip select pin for the SD card module
+#define LOG_INTERVAL_MS 1000  // Logging interval in milliseconds (15 seconds in this example)
+#define ADC_READS 34           // Number of ADC reads to average
+#define WRITE_LOG_TO_SERIAL true  // write the logged data to the serial port 
+#define USE_SD_CARD true        // log the data to the sd card
+
+// measurement Resistore
+#define CURRENT_SHUNT_OHMS  5.0  // value of resistor being used to sense current
+#define VOLTAGE_DIVIDER_R1_OHMS 15000
+#define VOLTAGE_DIVIDER_R2_OHMS 1000
+
+// Variables for battery measurement
+const int batteryPin = A0;
+const int currentPin = A1;
+
+String logFileName;
+
+// Variables for timekeeping
+unsigned long previousMillis = 0;
+
+// Variables to store measurements to average
+uint16_t batteryRaw[ADC_READS], currentRaw[ADC_READS];
+
+
+void setup() {
+  Serial.begin(9600);
+  analogReference(INTERNAL);  // set 1.1V internal reference
+
+  if (USE_SD_CARD){ 
+    // Initialize SD card
+   if (!SD.begin(SD_CS_PIN)) {
+     Serial.println("SD card initialization failed!");
+     return;
+   }
+    // Create a new file for logging
+    logFileName = getLogFileName();
+
+    File dataFile = SD.open(logFileName, FILE_WRITE);
+    if (dataFile) {
+      dataFile.println("Time,Voltage(mV),current(mA)");
+      dataFile.close();
+    } else {
+      Serial.println("Error creating log file!");
+    }
+  }
+}
+
+void loop() {
+ 
+  if(millis() - previousMillis > LOG_INTERVAL_MS) // if time elapsed
+  {
+    // Update the previousMillis variable
+    previousMillis = millis();
+    // Calculate the battery voltage
+    for(int n=0;n<ADC_READS;n++){ 
+      currentRaw[n] = analogRead(currentPin);
+      batteryRaw[n] = analogRead(batteryPin);  
+    }
+
+    // Log the data to the SD card
+    logData(previousMillis, calculateAverage(batteryRaw, ADC_READS), calculateAverage(currentRaw, ADC_READS) );
+  }
+
+}
+
+uint16_t calculateAverage(uint16_t data[], int count){
+  int highestValueIndex = 0, lowestValueIndex = 0, n;
+  uint32_t total = 0;
+  if(n<=0)
+    return 0; // invalid input data
+
+  if(n<4) {
+    // if not enough values to remove highest and lowest and average
+    for(n=0;n<count;n++)
+      total = total + data[n];
+    return (uint16_t)(total/count);
+  }
+
+  for(n=1;n<count;n++){
+    if(data[n] > data[highestValueIndex])
+      highestValueIndex = n;
+    if(data[n] < data[lowestValueIndex])
+      lowestValueIndex = n;
+  }
+  int countedValues = 0;
+  for(n=0;n<count;n++){
+    if(n!= lowestValueIndex && n!= highestValueIndex){
+      total+= data[n];
+      countedValues++;  // Use this for case of highest and lowest being both the same to avoid bug from count-2
+    }
+  }
+  return (uint16_t)(total/countedValues);
+
+}
+
+
+uint16_t rawToBatteryVoltagemv(uint16_t raw) {
+  //                          mv/ADCunit        (R1 + R2) / R2
+  float voltage = raw * (1100.0 / 1023.0) * (float)(VOLTAGE_DIVIDER_R1_OHMS + VOLTAGE_DIVIDER_R2_OHMS)/(float)VOLTAGE_DIVIDER_R2_OHMS;
+  return (uint16_t) voltage;
+}
+
+uint16_t rawToCurrentMa(uint16_t raw){
+  uint16_t result;
+  result = (uint16_t)((float)raw *(1100.0 / 1023.0) / CURRENT_SHUNT_OHMS);
+  return result;
+}
+
+String millisToHMS(unsigned long millis){
+  unsigned long seconds = millis / 1000;
+  unsigned long minutes = seconds / 60;
+  unsigned long hours = minutes / 60;
+
+  seconds %= 60;
+  minutes %= 60;
+ 
+  return String( String(hours) + ":" + String(minutes) + ":" + String(seconds) );
+}
+
+void logData(unsigned long timestamp, uint16_t rawvoltage, uint16_t rawcurrent) {
+  String timestampString = millisToHMS(timestamp);
+  String logLine = String(timestampString + "," + String(rawToBatteryVoltagemv(rawvoltage)) + "," + String(rawToCurrentMa(rawcurrent)) + "\n");
+
+  if(WRITE_LOG_TO_SERIAL)
+    Serial.print(logLine);
+  
+  if(USE_SD_CARD){ 
+    File dataFile = SD.open(logFileName, FILE_WRITE | O_CREAT | O_APPEND);
+    if (dataFile) {
+      dataFile.print(logLine);
+      Serial.println("Data logged.");
+      dataFile.close();
+    } else {
+      Serial.println("Error opening log file!");
+    }
+  }
+}
+
+String getLogFileName() {
+  String baseFileName = "log";
+  String fileExt = ".csv";
+  String fileName = baseFileName + "01" + fileExt;
+  int fileCount = 1;
+
+  while (SD.exists(fileName)) {
+    // Generate a new file name with a numbered suffix
+    if(fileCount < 10){
+    fileName = baseFileName +  "0" + String(fileCount) + fileExt;
+    }else{
+    fileName = baseFileName + String(fileCount) + fileExt;    
+    }
+    fileCount++;
+  }
+
+  return fileName;
+}
